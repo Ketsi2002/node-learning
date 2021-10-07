f433f31c49c738d7f7724110fbd2ac524d9ade4d
Moeketsi <moeketsi.tsoaeli@gmail.com>
2021-10-08 01:13:22 +0200

    first commit

---
+++ app.js
@@ -0,0 +1,21 @@
+// npm -- global command, comes with node
+// npm --v
+
+// local dependency - use it only in this particular project
+// npm i <packageName>
+
+// global dependency - use it in any project
+// npm install -g <packageName>
+// sudo npm install -g <packageName>
+
+// package.json - manifest file (stores important info about project/package)
+// manual approach (create package.json in the root , create properties etc)
+// npm init (step by step, press enter to skip) see 020 first packages and package.json Video.
+
+// npm init -y (everything default)
+
+const _ = require('lodash');
+
+const items = [1, [2,[3,[4]]]]
+const newItems = _.flattenDeep(items);
+console.log(newItems);
